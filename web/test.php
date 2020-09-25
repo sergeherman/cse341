78eba94798b8943fae81bf8707bfd8cc4c778093
Sergey Herman <HermanSP@corp.oakmgmt.ru>
2020-09-26 00:17:19 +0300

    message

---
+++ web/test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>cse341 week 2 enhancement</title>
+
+    </head>
+    <body>
+    
+    <header>
+      <div class="clearfix">
+        <span class="float-right-sm">My Personal Homepage</span>
+        <span class="float-right-lg">Charlie Chaplin</span><br>
+      </div>
+      <div class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#Movies">Movies</a>
+        <a href="#Biograhy">Biograhy</a>
+        <a href="#Media">Media</a>
+        <a href="#contact">Contact</a>
+        
+              </div>
+    </header>
+    <main>
+    <div style="padding-left:20px">
+      <p>Content goes here</p>
+     
+    </div>
+
+    </main>
+    <footer class="example">footer text
+    
+  
+    <?php
+echo "The server time is " . date("h:i:sa");
+
+?>
+	</footer>
+    
+    </body>
+    </html>
+    
+ 
+</body>
+</html>
