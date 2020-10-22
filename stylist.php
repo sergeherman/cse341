8af9604cda8cea45de1a0425cbe7787d2a308c99
Siarhei Herman <ser.herman@gmail.com>
2020-10-22 15:24:47 +0300

    message

--- stylist.php
+++ stylist.php
@@ -1,31 +1,42 @@
 <?php
-include_once 'mydbtest/dbh.mydbtest.php';
+require "dbconnect.php";
+$db = get_db();
+
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
- <tytle></tytle>
- </head>
- <body>
- <?php
-$sql = "select firstName, secondName, gender, email
-from stylist 
-order by secondName";
-$result = mysqli_query($conn, $sql);
-$resultCheck = mysqli_num_rows($result);
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 
-echo " Stylists list:"."<br>";
+<?php
+echo " STYLITS LIST:"."<br>";
+try {
 
-if ($resultCheck >0) {
-	while ($row = mysqli_fetch_assoc($result)) {
-		echo $row['firstName']."   ";
-		echo $row['secondName'].", ";
-		echo $row['gender'].", ";
-		echo $row['email']."<br>";
+  $statement = $db->prepare('SELECT firstname , secondname , gender, email from stylist order by secondname;');
+  $statement->execute();
+
+  while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $firstname = $row['firstname'];
+    $secondname = $row['secondname'];
+    $gender = $row['gender'];
+    $email = $row['email'];
+
+    echo "<p> Stylist name: $firstname  $secondname, Gender:  $gender, Email:  $email</p>";
+
+  }
+} catch (Exception $ex) {
+  echo "$ex";
 }
-}
+
+
+
+
 ?>
  
- </body>
- </html>
+</body>
+</html>
