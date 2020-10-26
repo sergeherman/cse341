c5e8d20d5ac1e131d4178a673ab19ffc3c652ca5
Siarhei Herman <ser.herman@gmail.com>
2020-10-26 13:59:43 +0300

    message

--- web/client.php
+++ web/client.php
@@ -14,13 +14,13 @@ $db = get_db();
 <body>
 <h2> CLIENTS LIST<h2> 
 
-<form method="post" action="addclient.php">
+<!-- <form method="post" action="addclient.php">
 <textarea name = 'firstname'><textarea>
 <textarea name = 'secondname'><textarea>
 <textarea name = 'gender'><textarea>
 <textarea name = 'email'><textarea>
 <input type='submit' value='Create Client'>
-</form>
+</form> -->
 <ul>
 <?php
 
