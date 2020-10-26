73e7936689894d300abd99c7d9118f57e48a8892
Siarhei Herman <ser.herman@gmail.com>
2020-10-26 16:54:44 +0300

    message

--- web/addorders.php
+++ web/addorders.php
@@ -1,5 +1,5 @@
 <?php
-$orderdate = htmlspecialchars($_POST['orderdate']);
+//$orderdate = htmlspecialchars($_POST['orderdate']);
 $client_id = htmlspecialchars($_POST['client_id']);
 $stylist_id = htmlspecialchars($_POST['stylist_id']);
 $service_id = htmlspecialchars($_POST['service_id']);
@@ -7,8 +7,8 @@ $service_id = htmlspecialchars($_POST['service_id']);
 require "dbconnect.php";
 $db = get_db();
 
-$stmt = $db->prepare('INSERT INTO orders (orderdate, client_id, stylist_id, service_id) VALUES (:orderdate, :client_id, :stylist_id, service_id);');
-$stmt->bindValue(':orderdate', $orderdate, PDO::PARAM_STR);
+$stmt = $db->prepare('INSERT INTO orders (client_id, stylist_id, service_id) VALUES (:client_id, :stylist_id, service_id);');
+//$stmt->bindValue(':orderdate', $orderdate, PDO::PARAM_STR);
 $stmt->bindValue(':client_id', $client_id, PDO::PARAM_INT);
 $stmt->bindValue(':stylist_id', $stylist_id, PDO::PARAM_INT);
 $stmt->bindValue(':service_id', $service_id, PDO::PARAM_INT);
