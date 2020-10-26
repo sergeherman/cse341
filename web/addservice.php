c585f987c0cef7157de1529f874bbdf3f96d38f6
Siarhei Herman <ser.herman@gmail.com>
2020-10-26 16:16:03 +0300

    message

--- web/addservice.php
+++ web/addservice.php
@@ -6,7 +6,7 @@ $product_id = htmlspecialchars($_POST['product_id']);
 require "dbconnect.php";
 $db = get_db();
 
-$stmt = $db->prepare('INSERT INTO stylist (servicename, service, product_id) VALUES (:servicename, :service, :gender, :email);');
+$stmt = $db->prepare('INSERT INTO service (servicename, service, product_id) VALUES (:servicename, :service, :gender, :email);');
 $stmt->bindValue(':servicename', $servicename, PDO::PARAM_STR);
 $stmt->bindValue(':service', $service, PDO::PARAM_STR);
 $stmt->bindValue(':product_id', $product_id, PDO::PARAM_INT);
