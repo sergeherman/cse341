5950853c62a5eb67b23d8745d61d79040fcb7d23
Siarhei Herman <ser.herman@gmail.com>
2020-10-28 16:07:44 +0300

    message

--- web/deleteclient.php
+++ web/deleteclient.php
@@ -3,7 +3,7 @@
 require "dbconnect.php";
 $db = get_db();
 
-$stmt = $db->prepare('DELETE FROM client WHERE id = MAX(id);');
+$stmt = $db->prepare('DELETE FROM client WHERE id = (select MAX(id) from client);');
 // $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
 // $stmt->bindValue(':secondname', $secondname, PDO::PARAM_STR);
 // $stmt->bindValue(':gender', $gender, PDO::PARAM_STR);
