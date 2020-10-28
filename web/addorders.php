ee6cd315b7d7b15f7243e74337f72ed20c49c07d
Siarhei Herman <ser.herman@gmail.com>
2020-10-28 14:26:11 +0300

    message

--- web/addorders.php
+++ web/addorders.php
@@ -6,17 +6,17 @@ $service_id = htmlspecialchars($_POST['service_id']);
 
 require "dbconnect.php";
 $db = get_db();
+echo "$orderdate $client_id $stylist_id $service_id"
+// $stmt = $db->prepare('INSERT INTO orders (client_id, service_id, stylist_id, orderdate ) VALUES (:client_id, :service_id, :stylist_id, :orderdate);');
+// $stmt->bindValue(':orderdate', $orderdate, PDO::PARAM_STR);
+// $stmt->bindValue(':client_id', $client_id, PDO::PARAM_INT);
+// $stmt->bindValue(':stylist_id', $stylist_id, PDO::PARAM_INT);
+// $stmt->bindValue(':service_id', $service_id, PDO::PARAM_INT);
+// $stmt->execute();
 
-$stmt = $db->prepare('INSERT INTO orders (client_id, service_id, stylist_id, orderdate ) VALUES (:client_id, :service_id, :stylist_id, :orderdate);');
-$stmt->bindValue(':orderdate', $orderdate, PDO::PARAM_STR);
-$stmt->bindValue(':client_id', $client_id, PDO::PARAM_INT);
-$stmt->bindValue(':stylist_id', $stylist_id, PDO::PARAM_INT);
-$stmt->bindValue(':service_id', $service_id, PDO::PARAM_INT);
-$stmt->execute();
+// $new_page = "orders.php";
 
-$new_page = "orders.php";
-
-header("Location: $new_page");
+// header("Location: $new_page");
 die();
 
 ?>
