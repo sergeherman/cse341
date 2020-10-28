c0af813e816e37260251c7f43df7845aac1ad477
Siarhei Herman <ser.herman@gmail.com>
2020-10-28 14:45:57 +0300

    message

--- web/client.php
+++ web/client.php
@@ -10,6 +10,23 @@ $db = get_db();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CLIENTS</title>
+  <style>
+body {font-family: arial, sans-serif;}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
 </head>
 <body>
 <h2> CLIENTS LIST<h2> 
